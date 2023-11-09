97a73d68c4c242a4f7e386b457af81dc33cf3102
renier1989 <renier1989@gmail.com>
2023-11-09 15:27:15 -0500

    326. Creando el Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { AppClima } from "./components/AppClima";
+import { ClimaProvider } from "./context/ClimaProvider";
 
 function App() {
-  return <AppClima/>;
+  return (
+    <ClimaProvider>
+      <header>
+        <h1>Buscador de Clima</h1>
+      </header>
+      <AppClima />;
+    </ClimaProvider>
+  );
 }
 
 export default App;
---
+++ src/context/ClimaProvider.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { createContext } from "react";
+
+const ClimaContext = createContext();
+
+const ClimaProvider = ({children}) => {
+
+    const [busqueda,setBusqueda] = useState({
+        ciudad: '',
+        pais : ''
+    })
+
+    const datosBusqueda = e => {
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return(
+        <ClimaContext.Provider value={{busqueda, datosBusqueda}}>
+            {children}
+        </ClimaContext.Provider>
+    )
+}
+
+export {ClimaProvider}
+
+export default ClimaContext;
