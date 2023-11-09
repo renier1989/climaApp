560ce1a151dba2969dc53281f4909d220ea05723
renier1989 <renier1989@gmail.com>
2023-11-09 15:19:03 -0500

    325. Creando el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,27 @@
 export const Formulario = () => {
-  return <div>Formulario</div>;
+  return <div className="contenedor">
+    <form >
+        <div className="campo">
+            <label htmlFor="ciudad" className="ciudad">Ciudad</label>
+            <input type="text"
+                id="ciudad"
+                name="ciudad"
+            />
+        </div>
+        <div className="campo">
+            <label htmlFor="ciudad" className="pais">País</label>
+            <select name="pais" id="pias">
+                <option value="">--Seleccione un País--</option>
+                <option value="US">Estado Unidos</option>
+                <option value="VE">Venezuela</option>
+                <option value="PE">Perú</option>
+                <option value="MX">Mexixo</option>
+                <option value="AR">Argentina</option>
+                <option value="CO">Colombia</option>
+                <option value="ES">España</option>
+            </select>
+        </div>
+        <input type="submit" value="Consultar Clima" />
+    </form>
+  </div>;
 };
